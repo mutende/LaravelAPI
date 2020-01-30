b02f40e05e1fefcdfabac5de4829c9bb8e199112
Elvis Mutende <elvismutende@gmail.com>
2020-01-30 12:52:35 -0500

    CRUDE backed API working

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get articles
+        $articles = Article::paginate(15);
+        //return a collection of articles as a resource
+        return ArticleResource::collection($articles);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = $request->isMethod('put') ?  Article::findorFail($request->id) : new Article;
+        $article->id = $request->input('id');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+
+        if($article->save()){
+            return new ArticleResource($article);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //get a single article
+
+        $article = Article::findorFail($id);
+        //return a single article
+        return new ArticleResource($article);
+    }
+
+    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+        $article = Article::findorFail($id);
+        if($article->delete()){
+            return new ArticleResource($article);
+        }
+        
+    }
+}
